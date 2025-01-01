79f6b57fb6793900308447f7e26f1db9ece1d29f
PerfectDay20 <zhang.stef@gmail.com>
2025-01-02 00:19:22 +0800

    quit when only nvim-tree left

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -19,6 +19,21 @@ return {
     "nvim-tree/nvim-tree.lua",
     cmd = { "NvimTreeToggle", "NvimTreeFocus", "NvimTreeOpen" },
     opts = function()
+      vim.api.nvim_create_autocmd("BufEnter", {
+        group = vim.api.nvim_create_augroup("NvimTreeClose", { clear = true }),
+        pattern = "NvimTree_*",
+        callback = function()
+          local layout = vim.api.nvim_call_function("winlayout", {})
+          if
+            layout[1] == "leaf"
+            and vim.api.nvim_get_option_value("filetype", { buf = vim.api.nvim_win_get_buf(layout[2]) }) == "NvimTree"
+            and layout[3] == nil
+          then
+            vim.cmd "confirm quit"
+          end
+        end,
+      })
+
       return require "nvchad.configs.nvimtree"
     end,
   },
