77d6e9364f80c02c928cb4d11519f65fb02018d7
PerfectDay20 <zhang.stef@gmail.com>
2025-01-01 10:36:47 +0800

    change cursor and scroll off

--- lua/options.lua
+++ lua/options.lua
@@ -2,18 +2,24 @@ require "nvchad.options"
 
 -- add yours here!
 
-local o = vim.o
-o.cursorlineopt = "both" -- to enable cursorline!
+vim.opt.cursorlineopt = "both" -- to enable cursorline!
 
 -- enable ssh remote copy and paste
 vim.g.clipboard = {
-  name = 'OSC 52',
+  name = "OSC 52",
   copy = {
-    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
-    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+    ["+"] = require("vim.ui.clipboard.osc52").copy "+",
+    ["*"] = require("vim.ui.clipboard.osc52").copy "*",
   },
   paste = {
-    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
-    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+    ["+"] = require("vim.ui.clipboard.osc52").paste "+",
+    ["*"] = require("vim.ui.clipboard.osc52").paste "*",
   },
 }
+
+-- change cursor color: https://neovim.io/doc/user/faq.html#faq
+vim.api.nvim_set_hl(0, "Cursor", { fg = "red", bg = "red" })
+vim.opt.guicursor="n-v-c-sm:block,i-ci-ve:ver25-Cursor,r-cr-o:hor20"
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
